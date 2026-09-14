5a7143fec07b195a7145292841a6782245fee86b
filbertik <aton2019zvit@gmail.com>
2026-09-14 15:54:36 +0300

    edit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -11,10 +13,23 @@ export default defineConfig({
   },
 });
 
-// import react from '@vitejs/plugin-react'
-// import { defineConfig } from 'vite'
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
 
-// // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-// })
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+// // import react from '@vitejs/plugin-react'
+// // import { defineConfig } from 'vite'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
