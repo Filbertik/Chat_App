02bb0ef8764fe0ca63b3874425ec8f8ee371c9dd
filbertik <aton2019zvit@gmail.com>
2026-09-15 08:11:48 +0300

    mes list

---
+++ src/components/chat/MessageList.tsx
@@ -0,0 +1,26 @@
+import { messages } from "@/data/mockChats";
+import Message from "./Message";
+
+interface MessageListProps {
+  chatId: string;
+}
+
+const MessageList = ({ chatId }: MessageListProps) => {
+  const chatMessages = messages.filter((message) => message.chatId === chatId);
+
+  return (
+    <div className="flex flex-1 flex-col overflow-y-auto px-8 py-6">
+      <div className="flex flex-col gap-4">
+        {chatMessages.map((message) => (
+          <Message
+            key={message.id}
+            message={message}
+            isOwn={message.senderId === "current-user"}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MessageList;
