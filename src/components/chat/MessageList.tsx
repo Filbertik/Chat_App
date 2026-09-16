b5f534e59aaeb7ba52eefc7c48195feb6744df3d
filbertik <aton2019zvit@gmail.com>
2026-09-16 14:57:59 +0300

    msg list

--- src/components/chat/MessageList.tsx
+++ src/components/chat/MessageList.tsx
@@ -1,4 +1,5 @@
-import { messages } from "@/data/mockChats";
+import { useChatStore } from "@/store/chatStore";
+
 import Message from "./Message";
 
 interface MessageListProps {
@@ -6,6 +7,8 @@ interface MessageListProps {
 }
 
 const MessageList = ({ chatId }: MessageListProps) => {
+  const messages = useChatStore((state) => state.messages);
+
   const chatMessages = messages.filter((message) => message.chatId === chatId);
 
   return (
@@ -24,3 +27,30 @@ const MessageList = ({ chatId }: MessageListProps) => {
 };
 
 export default MessageList;
+
+// import { messages } from "@/data/mockChats";
+// import Message from "./Message";
+
+// interface MessageListProps {
+//   chatId: string;
+// }
+
+// const MessageList = ({ chatId }: MessageListProps) => {
+//   const chatMessages = messages.filter((message) => message.chatId === chatId);
+
+//   return (
+//     <div className="flex flex-1 flex-col overflow-y-auto px-8 py-6">
+//       <div className="flex flex-col gap-4">
+//         {chatMessages.map((message) => (
+//           <Message
+//             key={message.id}
+//             message={message}
+//             isOwn={message.senderId === "current-user"}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MessageList;
