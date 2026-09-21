6607ab895b0d2e718b99aa7ce8e273176e901c09
filbertik <aton2019zvit@gmail.com>
2026-09-21 10:20:35 +0300

    add types

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -13,6 +13,7 @@ export interface Message {
   text: string;
   createdAt: string;
   isRead: boolean;
+  isEdited: boolean;
 }
 
 export interface Chat {
@@ -21,49 +22,3 @@ export interface Chat {
   lastMessage: Message;
   unreadCount: number;
 }
-
-// export interface User {
-//   id: string;
-//   name: string;
-//   avatar: string;
-//   isOnline: boolean;
-// }
-
-// export interface Message {
-//   id: string;
-//   chatId: string;
-//   senderId: string;
-//   text: string;
-//   createdAt: string;
-//   isRead: boolean;
-// }
-
-// export interface Chat {
-//   id: string;
-//   user: User;
-//   lastMessage: Message;
-//   unreadCount: number;
-// }
-
-// // export interface User {
-// //   id: string;
-// //   name: string;
-// //   avatar: string;
-// //   isOnline: boolean;
-// // }
-
-// // export interface Message {
-// //   id: string;
-// //   chatId: string;
-// //   senderId: string;
-// //   text: string;
-// //   createdAt: string;
-// //   isRead: boolean;
-// // }
-
-// // export interface Chat {
-// //   id: string;
-// //   user: User;
-// //   lastMessage: Message;
-// //   unreadCount: number;
-// // }
