fd76604fa33313f55cc119d4487ea9f5a242a40e
filbertik <aton2019zvit@gmail.com>
2026-09-22 11:39:57 +0300

    fix msg list

--- src/components/chat/MessageList.tsx
+++ src/components/chat/MessageList.tsx
@@ -12,7 +12,7 @@ const MessageList = ({ chatId }: MessageListProps) => {
   const chatMessages = messages.filter((message) => message.chatId === chatId);
 
   return (
-    <div className="flex flex-1 flex-col overflow-y-auto px-8 py-6">
+    <div className="flex flex-1 flex-col overflow-y-auto px-4 py-5 md:px-8 md:py-6">
       <div className="flex flex-col gap-4">
         {chatMessages.map((message) => (
           <Message
@@ -27,30 +27,3 @@ const MessageList = ({ chatId }: MessageListProps) => {
 };
 
 export default MessageList;
-
-// import { messages } from "@/data/mockChats";
-// import Message from "./Message";
-
-// interface MessageListProps {
-//   chatId: string;
-// }
-
-// const MessageList = ({ chatId }: MessageListProps) => {
-//   const chatMessages = messages.filter((message) => message.chatId === chatId);
-
-//   return (
-//     <div className="flex flex-1 flex-col overflow-y-auto px-8 py-6">
-//       <div className="flex flex-col gap-4">
-//         {chatMessages.map((message) => (
-//           <Message
-//             key={message.id}
-//             message={message}
-//             isOwn={message.senderId === "current-user"}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MessageList;
